efd6820ca6e2ef3896c6d1887cffc66dbe69b7a4
Emma <emmaomoide@gmail.com>
2017-12-29 12:24:11 +0700

    Doing ultrasonic instead.

--- a00_keypad/coba_00/main.c
+++ a00_keypad/coba_00/main.c
@@ -17,10 +17,26 @@ Please refer to LICENSE file for licensing information.
 #include <string.h>
 
 #include "lcdpcf8574/lcdpcf8574.h"
+#include "Keypad/Keypad.h"
 
 #define UART_BAUD_RATE 9600
 //#define F_CPU 16000000
 #include "uart/uart.h"
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'0','1','2','3'},
+  {'4','5','6','7'},
+  {'8','9','A','B'},
+  {'C','D','E','F'}
+};
+byte rowPins[ROWS] = {3, 2, 1, 0}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 void display_main(uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second, uint8_t rate, uint8_t height, uint8_t relay);
 
@@ -58,7 +74,14 @@ int main(void)
 		
 		display_main(month, day, hour, minute, second, rate, height, relay);
 		
-		_delay_ms(500);
+		
+		char customKey = customKeypad.getKey();
+
+		if (customKey){
+		Serial.println(customKey);
+		}
+		
+		_delay_ms(100);
 		count++;
     }
 }
